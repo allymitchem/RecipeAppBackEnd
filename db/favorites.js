9287b5b322f9f7ada3f9056fe854f4236452a03c
Allyson Mitchem <allysonmitchem@gmail.com>
2023-02-08 10:34:14 -0700

    created favorites.js but needing to possibly restructure favorites table to be separate/joined to a table that includes the recipe info

---
+++ db/favorites.js
@@ -0,0 +1,8 @@
+const client = require('./client')
+/* need to figure out how we want the favorites table to be structured..
+all in one table or joined with a table that houses the recipe info */
+
+
+// async function addFavorite(){
+
+// }
